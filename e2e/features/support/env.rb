db42e98150e8f4f4e2930b655d57bee2111f1f35
Lucas Avila <lavila@thoughtworks.com>
2018-07-01 21:22:51 -0300

    Adds html reports and screenshots

--- e2e/features/support/env.rb
+++ e2e/features/support/env.rb
@@ -2,6 +2,7 @@ require 'capybara/cucumber'
 require 'site_prism'
 require 'pry'
 require 'rspec'
+require 'capybara-screenshot/cucumber'
 
 Capybara.javascript_driver = :chrome
 Capybara.register_driver :selenium do |app|
@@ -10,3 +11,5 @@ end
 Capybara.default_driver = :selenium
 Capybara.page.driver.browser.manage.window.maximize
 Capybara.default_max_wait_time = 5
+Capybara.save_path = "screenshots"
+Capybara::Screenshot.prune_strategy = :keep_last_run
